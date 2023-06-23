95eab2a9212ebfe850b4f30adc9e3067e0a32cb8
Spyros Koukas <21125056+SpyrosKou@users.noreply.github.com>
2023-06-23 18:50:02 +0300

    Add logging messages

--- rosjava/src/main/java/org/ros/RosRun.java
+++ rosjava/src/main/java/org/ros/RosRun.java
@@ -24,6 +24,10 @@ import org.ros.node.DefaultNodeMainExecutor;
 import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMain;
 import org.ros.node.NodeMainExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
 
 /**
  * This is a main class entry point for executing {@link NodeMain}s.
@@ -32,6 +36,7 @@ import org.ros.node.NodeMainExecutor;
  * @author damonkohler@google.com (Damon Kohler)
  */
 public final class RosRun {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     public static void printUsage() {
         System.err.println("Usage: java -jar my_package.jar com.example.MyNodeMain [args]");
@@ -52,14 +57,20 @@ public final class RosRun {
         try {
             nodeMain = loader.loadClass(nodeClassName);
         } catch (ClassNotFoundException e) {
-            throw new RosRuntimeException("Unable to locate node: " + nodeClassName, e);
+            final String msg = "Arguments:" + argv + " Unable to locate node: " + nodeClassName;
+            LOGGER.error(msg);
+            throw new RosRuntimeException(msg, e);
         } catch (InstantiationException e) {
-            throw new RosRuntimeException("Unable to instantiate node: " + nodeClassName, e);
+            final String msg = "Arguments:" + argv + " Unable to instantiate node: " + nodeClassName;
+            LOGGER.error(msg);
+            throw new RosRuntimeException(msg, e);
         } catch (IllegalAccessException e) {
-            throw new RosRuntimeException("Unable to instantiate node: " + nodeClassName, e);
+            final String msg = "Arguments:" + argv + " Unable to instantiate node: " + nodeClassName;
+            LOGGER.error(msg);
+            throw new RosRuntimeException(msg, e);
         }
 
-        Preconditions.checkState(nodeMain != null);
+        Preconditions.checkState(nodeMain != null, "nodeMain was null");
         final NodeMainExecutor nodeMainExecutor = DefaultNodeMainExecutor.newDefault();
         nodeMainExecutor.execute(nodeMain, nodeConfiguration);
     }
