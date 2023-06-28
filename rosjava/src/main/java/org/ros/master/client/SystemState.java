15a741008de3c2dee5719ae7c18f8b7287dc7694
Spyros Koukas <21125056+SpyrosKou@users.noreply.github.com>
2023-06-28 22:49:55 +0300

    Collection->Set, add equals/hashcode

--- rosjava/src/main/java/org/ros/master/client/SystemState.java
+++ rosjava/src/main/java/org/ros/master/client/SystemState.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2012 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,30 +16,56 @@
 
 package org.ros.master.client;
 
-import java.util.Collection;
+import java.util.*;
 
 /**
  * The state of the ROS graph as understood by the master.
- * 
+ *
  * @author Keith M. Hughes
  */
 public final class SystemState {
 
-	/**
-	 * All topics known.
-	 */
-	private final Collection<TopicSystemState> topics;
+    /**
+     * All topics known.
+     */
+    private final Set<TopicSystemState> topics;
 
-	public SystemState(Collection<TopicSystemState> topics) {
-		this.topics = topics;
-	}
+    public SystemState(final Collection<TopicSystemState> topics) {
+        if (topics != null) {
+            this.topics = Set.copyOf(topics);
+        } else {
+            this.topics = Collections.emptySet();
+        }
+    }
 
-	/**
-	 * Get all topics in the system state.
-	 * 
-	 * @return a collection of topics.
-	 */
-	public Collection<TopicSystemState> getTopics() {
-		return topics;
-	}
+    /**
+     * Get all topics in the system state.
+     *
+     * @return a collection of topics.
+     */
+    public final Set<TopicSystemState> getTopics() {
+        return topics;
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final SystemState that = (SystemState) o;
+        return getTopics().equals(that.getTopics());
+    }
+
+
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(getTopics());
+    }
+    @Override
+    public final String toString() {
+        return "SystemState{" +
+                "topics=" + topics +
+                '}';
+    }
+
 }
