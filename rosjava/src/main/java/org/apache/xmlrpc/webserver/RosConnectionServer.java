128e64d9371e4a4aa05b930b017a7cf0033a9024
Spyros Koukas <21125056+SpyrosKou@users.noreply.github.com>
2024-03-18 05:21:28 +0200

    Use only Object[] results (no intermediate list) , named static inner class instead of anonymous,

--- rosjava/src/main/java/org/apache/xmlrpc/webserver/RosConnectionServer.java
+++ rosjava/src/main/java/org/apache/xmlrpc/webserver/RosConnectionServer.java
@@ -15,60 +15,65 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * @author Spyros Koukas
  */
 public final class RosConnectionServer extends ConnectionServer {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final String METHOD_NAME = "methodName";
+    private static final String PARAMS = "params";
+    private static final String SYSTEM_MULTICALL = "system.multicall";
 
     /**
      * Returns, whether the
      * /** Processes a "connection". The "connection" is an opaque object, which is
      * being handled by the subclasses.
      *
-     * @param pConfig     The request configuration.
-     * @param pConnection The "connection" being processed.
+     * @param xmlRpcStreamRequestConfig     The request configuration.
+     * @param serverStreamConnection The "connection" being processed.
      * @throws XmlRpcException Processing the request failed.
      */
     public final void execute(
-            final XmlRpcStreamRequestConfig pConfig
-            , final ServerStreamConnection pConnection)
+            final XmlRpcStreamRequestConfig xmlRpcStreamRequestConfig
+            , final ServerStreamConnection serverStreamConnection)
             throws XmlRpcException {
         try {
             if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("Starting...");
+                LOGGER.debug("Execution started.");
             }
             Object result = null;
             Throwable error = null;
 
-            try (final InputStream inputStream = this.getInputStream(pConfig, pConnection)) {
-                final XmlRpcRequest request = this.getRequest(pConfig, inputStream);
-                if (request.getMethodName().equals("system.multicall")) {
+            try (final InputStream inputStream = this.getInputStream(xmlRpcStreamRequestConfig, serverStreamConnection)) {
+                final XmlRpcRequest request = this.getRequest(xmlRpcStreamRequestConfig, inputStream);
+                if (SYSTEM_MULTICALL.equals(request.getMethodName())) {
                     result = this.executeMulticall(request);
                 } else {
                     result = this.execute(request);
                 }
 
                 if (LOGGER.isDebugEnabled()) {
-                    LOGGER.debug("execute: Request performed successfully");
+                    LOGGER.debug("Execute: Request performed successfully");
                 }
             } catch (final Throwable throwable) {
                 this.logError(throwable);
                 error = throwable;
             }
-            final boolean contentLengthRequired = this.isContentLengthRequired(pConfig);
-            try (final OutputStream intermediateOutputStream = this.createIntermediateOutputStream(contentLengthRequired, pConnection);
-                 final OutputStream outputStream = this.getOutputStream(pConnection, pConfig, intermediateOutputStream);) {
+            final boolean contentLengthRequired = this.isContentLengthRequired(xmlRpcStreamRequestConfig);
+            try (final OutputStream intermediateOutputStream = this.createIntermediateOutputStream(contentLengthRequired, serverStreamConnection);
+                 final OutputStream outputStream = this.getOutputStream(serverStreamConnection, xmlRpcStreamRequestConfig, intermediateOutputStream);) {
                 if (error == null) {
-                    this.writeResponse(pConfig, outputStream, result);
+                    this.writeResponse(xmlRpcStreamRequestConfig, outputStream, result);
                 } else {
-                    this.writeError(pConfig, outputStream, error);
+                    this.writeError(xmlRpcStreamRequestConfig, outputStream, error);
                 }
                 if (contentLengthRequired) {
                     final ByteArrayOutputStream byteArrayOutputStream = (ByteArrayOutputStream) intermediateOutputStream;
-                    try (final OutputStream destinationOutputStream = super.getOutputStream(pConfig, pConnection, byteArrayOutputStream.size())) {
+                    try (final OutputStream destinationOutputStream = super.getOutputStream(xmlRpcStreamRequestConfig, serverStreamConnection, byteArrayOutputStream.size())) {
                         byteArrayOutputStream.writeTo(destinationOutputStream);
                     }
                 }
@@ -85,7 +90,7 @@ public final class RosConnectionServer extends ConnectionServer {
         } finally {
 
             try {
-                pConnection.close();
+                serverStreamConnection.close();
             } catch (final Throwable throwable) {
                 if (LOGGER.isDebugEnabled()) {
                     LOGGER.debug("Ignorable Throwable while trying to close connection:" + ExceptionUtils.getStackTrace(throwable));
@@ -94,25 +99,26 @@ public final class RosConnectionServer extends ConnectionServer {
 
         }
         if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("Ended.");
+            LOGGER.debug("Execution ended.");
         }
     }
 
     /**
      * @param contentLengthRequired
-     * @param pConnection           non closed connection, this method does not close it
+     * @param serverStreamConnection A non closed connection, this method does not close it
      * @return
      */
     private final OutputStream createIntermediateOutputStream(
             final boolean contentLengthRequired
-            , final ServerStreamConnection pConnection) throws IOException {
+            , final ServerStreamConnection serverStreamConnection) throws IOException {
         final OutputStream result;
         if (contentLengthRequired) {
             result = new ByteArrayOutputStream();
         } else {
-            result = pConnection.newOutputStream();
+            result = serverStreamConnection.newOutputStream();
         }
         return result;
+
     }
 
     private final Object[] executeMulticall(final XmlRpcRequest pRequest) {
@@ -120,48 +126,89 @@ public final class RosConnectionServer extends ConnectionServer {
             return null;
         }
 
+
         final Object[] reqs = (Object[]) pRequest.getParameter(0); // call requests
-        final ArrayList<Object> results = new ArrayList<>(); // call results
+        final Object[] results = new Object[reqs.length]; // call results
         final XmlRpcRequestConfig pConfig = pRequest.getConfig();
         // TODO: make concurrent calls?
         for (int i = 0; i < reqs.length; i++) {
-            Object result = null;
+
             try {
                 @SuppressWarnings("unchecked") final HashMap<String, Object> req = (HashMap<String, Object>) reqs[i];
-                final String methodName = (String) req.get("methodName");
-                final Object[] params = (Object[]) req.get("params");
-                result = this.execute(new XmlRpcRequest() {
-                    @Override
-                    public final XmlRpcRequestConfig getConfig() {
-                        return pConfig;
-                    }
+                final String methodName = (String) req.get(METHOD_NAME);
+                final Object[] params = (Object[]) req.get(PARAMS);
+                final RosConnectionXmlRpcRequest rosConnectionXmlRpcRequest = new RosConnectionXmlRpcRequest(methodName, pConfig, params);
+                results[i] = this.execute(rosConnectionXmlRpcRequest);
 
-                    @Override
-                    public final String getMethodName() {
-                        return methodName;
-                    }
-
-                    @Override
-                    public final int getParameterCount() {
-                        return params == null ? 0 : params.length;
-                    }
-
-                    @Override
-                    public final Object getParameter(int pIndex) {
-                        return params[pIndex];
-                    }
-                });
             } catch (final Throwable throwable) {
                 this.logError(throwable);
                 if (LOGGER.isErrorEnabled()) {
                     LOGGER.error("Ignorable:" + ExceptionUtils.getStackTrace(throwable));
                 }
                 // TODO: should this return an XmlRpc fault?
-                result = null;
+                results[i] = null;
             }
-            results.add(result);
+
         }
-        final Object[] retobj = new Object[]{results};
-        return retobj;
+        return results;
+    }
+
+    private static final class RosConnectionXmlRpcRequest implements XmlRpcRequest {
+        private final String methodName;
+        private final XmlRpcRequestConfig pConfig;
+
+
+
+        private final Object[] params;
+
+        RosConnectionXmlRpcRequest(final String methodName, final XmlRpcRequestConfig pConfig, final Object[] params) {
+            this.methodName = methodName;
+            this.pConfig = pConfig;
+            this.params = params;
+        }
+
+        @Override
+        public final XmlRpcRequestConfig getConfig() {
+            return this.pConfig;
+        }
+
+        @Override
+        public final String getMethodName() {
+            return this.methodName;
+        }
+
+        @Override
+        public final int getParameterCount() {
+            return this.params == null ? 0 : this.params.length;
+        }
+
+        @Override
+        public final Object getParameter(final int parameterIndex) {
+            return this.params[parameterIndex];
+        }
+
+        @Override
+        public final boolean equals(Object object) {
+            if (this == object) return true;
+            if (object == null || getClass() != object.getClass()) return false;
+            final RosConnectionXmlRpcRequest that = (RosConnectionXmlRpcRequest) object;
+            return Objects.equals(methodName, that.methodName) && Objects.equals(pConfig, that.pConfig) && Arrays.equals(params, that.params);
+        }
+
+        @Override
+        public final int hashCode() {
+            int result = Objects.hash(methodName, pConfig);
+            result = 31 * result + Arrays.hashCode(params);
+            return result;
+        }
+
+        @Override
+        public final String toString() {
+            return "RosConnectionXmlRpcRequest{" +
+                    "methodName='" + methodName + '\'' +
+                    ", pConfig=" + pConfig +
+                    ", params=" + Arrays.toString(params) +
+                    '}';
+        }
     }
 }
