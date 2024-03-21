76427f6471bc4bc48b0ec0c37a3d8fe5565e8c16
Spyros Koukas <21125056+SpyrosKou@users.noreply.github.com>
2024-03-21 15:06:19 +0200

    Log when an element is erased

--- rosjava/src/main/java/org/ros/concurrent/CircularBlockingDeque.java
+++ rosjava/src/main/java/org/ros/concurrent/CircularBlockingDeque.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2012 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,6 +16,10 @@
 
 package org.ros.concurrent;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
@@ -23,199 +27,212 @@ import java.util.NoSuchElementException;
  * A deque that removes head or tail elements when the number of elements
  * exceeds the limit and blocks on {@link #takeFirst()} and {@link #takeLast()} when
  * there are no elements available.
- * 
+ *
  * @author damonkohler@google.com (Damon Kohler)
  */
-public class CircularBlockingDeque<T> implements Iterable<T> {
+public final class CircularBlockingDeque<T> implements Iterable<T> {
+    private final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private final T[] queue;
+    private final Object mutex;
 
-  private final T[] deque;
-  private final Object mutex;
+    /**
+     * The maximum number of entries in the queue.
+     */
+    private final int limit;
 
-  /**
-   * The maximum number of entries in the queue.
-   */
-  private final int limit;
+    /**
+     * Points to the next entry that will be returned by {@link #takeFirst()} unless
+     * {@link #isEmpty()}.
+     */
+    private int start;
 
-  /**
-   * Points to the next entry that will be returned by {@link #takeFirst()} unless
-   * {@link #isEmpty()}.
-   */
-  private int start;
+    /**
+     * The number of entries in the queue.
+     */
+    private int length;
 
-  /**
-   * The number of entries in the queue.
-   */
-  private int length;
-
-  /**
-   * @param capacity
-   *          the maximum number of elements allowed in the queue
-   */
-  @SuppressWarnings("unchecked")
-  public CircularBlockingDeque(int capacity) {
-    deque = (T[]) new Object[capacity];
-    mutex = new Object();
-    limit = capacity;
-    start = 0;
-    length = 0;
-  }
-
-  /**
-   * Adds the specified entry to the tail of the queue, overwriting older
-   * entries if necessary.
-   * 
-   * @param entry
-   *          the entry to add
-   * @return {@code true}
-   */
-  public boolean addLast(T entry) {
-    synchronized (mutex) {
-      deque[(start + length) % limit] = entry;
-      if (length == limit) {
-        start = (start + 1) % limit;
-      } else {
-        length++;
-      }
-      mutex.notify();
+    /**
+     * @param capacity the maximum number of elements allowed in the queue
+     */
+    @SuppressWarnings("unchecked")
+    public CircularBlockingDeque(int capacity) {
+        this.queue = (T[]) new Object[capacity];
+        this.mutex = new Object();
+        this.limit = capacity;
+        this.start = 0;
+        this.length = 0;
     }
-    return true;
-  }
 
-  /**
-   * Adds the specified entry to the tail of the queue, overwriting older
-   * entries if necessary.
-   * 
-   * @param entry
-   *          the entry to add
-   * @return {@code true}
-   */
-  public boolean addFirst(T entry) {
-    synchronized (mutex) {
-      if (start - 1 < 0) {
-        start = limit - 1;
-      } else {
-        start--;
-      }
-      deque[start] = entry;
-      if (length < limit) {
-        length++;
-      }
-      mutex.notify();
-    }
-    return true;
-  }
+    /**
+     * Adds the specified entry to the tail of the queue, overwriting older
+     * entries if necessary.
+     *
+     * @param entry the entry to add
+     * @return {@code true}
+     */
+    public boolean addLast(T entry) {
+        synchronized (this.mutex) {
+            this.queue[(this.start + this.length) % this.limit] = entry;
+            if (this.length == this.limit) {
+                this.start = (this.start + 1) % this.limit;
 
-  /**
-   * Retrieves the head of the queue, blocking if necessary until an entry is
-   * available.
-   * 
-   * @return the head of the queue
-   * @throws InterruptedException
-   */
-  public T takeFirst() throws InterruptedException {
-    T entry;
-    synchronized (mutex) {
-      while (true) {
-        if (length > 0) {
-          entry = deque[start];
-          start = (start + 1) % limit;
-          length--;
-          break;
+                if (LOGGER.isDebugEnabled()) {
+                    final T deletedEntry = this.queue[start];
+                    LOGGER.debug(CircularBlockingDeque.class.getSimpleName() + " was full, an entry was overridden: {" + deletedEntry + "}");
+                }
+            } else {
+                this.length++;
+            }
+            this.mutex.notify();
         }
-        mutex.wait();
-      }
+        return true;
     }
-    return entry;
-  }
 
-  /**
-   * Retrieves, but does not remove, the head of this queue, returning
-   * {@code null} if this queue is empty.
-   * 
-   * @return the head of this queue, or {@code null} if this queue is empty
-   */
-  public T peekFirst() {
-    synchronized (mutex) {
-      if (length > 0) {
-        return deque[start];
-      }
-      return null;
-    }
-  }
-
-  /**
-   * Retrieves the tail of the queue, blocking if necessary until an entry is
-   * available.
-   * 
-   * @return the tail of the queue
-   * @throws InterruptedException
-   */
-  public T takeLast() throws InterruptedException {
-    T entry;
-    synchronized (mutex) {
-      while (true) {
-        if (length > 0) {
-          entry = deque[(start + length - 1) % limit];
-          length--;
-          break;
+    /**
+     * Adds the specified entry to the tail of the queue, overwriting older
+     * entries if necessary.
+     *
+     * @param entry the entry to add
+     * @return {@code true}
+     */
+    public boolean addFirst(T entry) {
+        synchronized (this.mutex) {
+            final int previousStart = this.start;
+            if (this.start - 1 < 0) {
+                this.start = this.limit - 1;
+            } else {
+                this.start--;
+            }
+            this.queue[this.start] = entry;
+            if (this.length < this.limit) {
+                this.length++;
+            } else {
+                if (LOGGER.isDebugEnabled()) {
+                    final T deletedEntry = this.queue[previousStart];
+                    LOGGER.debug(CircularBlockingDeque.class.getSimpleName() + " was full, an entry was overridden: {" + deletedEntry + "}");
+                }
+            }
+            this.mutex.notify();
         }
-        mutex.wait();
-      }
+        return true;
     }
-    return entry;
-  }
 
-  /**
-   * Retrieves, but does not remove, the tail of this queue, returning
-   * {@code null} if this queue is empty.
-   * 
-   * @return the tail of this queue, or {@code null} if this queue is empty
-   */
-  public T peekLast() {
-    synchronized (mutex) {
-      if (length > 0) {
-        return deque[(start + length - 1) % limit];
-      }
-      return null;
-    }
-  }
+    /**
+     * Retrieves the head of the queue, blocking if necessary until an entry is
+     * available.
+     *
+     * @return the head of the queue
+     * @throws InterruptedException
+     */
+    public T takeFirst() throws InterruptedException {
 
-  public boolean isEmpty() {
-    return length == 0;
-  }
-
-  /**
-   * Returns an iterator over the queue.
-   * <p>
-   * Note that this is not thread-safe and that {@link Iterator#remove()} is
-   * unsupported.
-   * 
-   * @see java.lang.Iterable#iterator()
-   */
-  @Override
-  public Iterator<T> iterator() {
-    return new Iterator<T>() {
-      int offset = 0;
-
-      @Override
-      public boolean hasNext() {
-        return offset < length;
-      }
-
-      @Override
-      public T next() {
-        if (offset == length) {
-          throw new NoSuchElementException();
+        synchronized (this.mutex) {
+            while (true) {
+                if (this.length > 0) {
+                    final T entry = this.queue[start];
+                    this.start = (this.start + 1) % this.limit;
+                    this.length--;
+                    return entry;
+                } else {
+                    this.mutex.wait();
+                }
+            }
         }
-        T entry = deque[(start + offset) % limit];
-        offset++;
-        return entry;
-      }
 
-      @Override
-      public void remove() {
-        throw new UnsupportedOperationException();
-      }
-    };
-  }
+    }
+
+    /**
+     * Retrieves, but does not remove, the head of this queue, returning
+     * {@code null} if this queue is empty.
+     *
+     * @return the head of this queue, or {@code null} if this queue is empty
+     */
+    public T peekFirst() {
+        synchronized (this.mutex) {
+            if (this.length > 0) {
+                return this.queue[this.start];
+            } else {
+                return null;
+            }
+        }
+    }
+
+    /**
+     * Retrieves the tail of the queue, blocking if necessary until an entry is
+     * available.
+     *
+     * @return the tail of the queue
+     * @throws InterruptedException
+     */
+    public T takeLast() throws InterruptedException {
+
+        synchronized (this.mutex) {
+            while (true) {
+                if (this.length > 0) {
+                    final T entry = this.queue[(this.start + this.length - 1) % this.limit];
+                    this.length--;
+                    return entry;
+                } else {
+                    this.mutex.wait();
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Retrieves, but does not remove, the tail of this queue, returning
+     * {@code null} if this queue is empty.
+     *
+     * @return the tail of this queue, or {@code null} if this queue is empty
+     */
+    public T peekLast() {
+        synchronized (mutex) {
+            if (length > 0) {
+                return queue[(start + length - 1) % limit];
+            } else {
+                return null;
+            }
+        }
+    }
+
+    public boolean isEmpty() {
+        return this.length == 0;
+    }
+
+    /**
+     * Returns an iterator over the queue.
+     * <p>
+     * Note that this is not thread-safe and that {@link Iterator#remove()} is
+     * unsupported.
+     *
+     * @see java.lang.Iterable#iterator()
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int offset = 0;
+
+            @Override
+            public final boolean hasNext() {
+                return offset < length;
+            }
+
+            @Override
+            public final T next() {
+                if (offset == length) {
+                    throw new NoSuchElementException();
+                } else {
+                    final T entry = queue[(start + offset) % limit];
+                    offset++;
+                    return entry;
+                }
+            }
+
+            @Override
+            public final void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+    }
 }
